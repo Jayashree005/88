1542b9258f5261b0cb46646387740c6811c5a8d6
Jayashree005 <71762305088@cit.edu.in>
2025-10-01 11:12:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var passport = require('passport');
+const cors = require('cors')
+var app = express();
+require('./auth/auth');
+const login = require('./routes/login')
+const loggedInPage = require('./routes/loggedInUser');
+const bookingRoute = require('./routes/routeSelection')
+var registerRouter = require('./routes/register');
+const DB_URL = require('./config/keys').MongoURI;
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log("Connected to MongoDB")
+    })
+    .catch(err => {
+        throw err
+    })
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+app.use('/', login);
+app.use('/booking', bookingRoute);
+app.use('/register', registerRouter);   
+app.use('/user', passport.authenticate('jwt', { session: false }), loggedInPage); 
+
+module.exports = app;
---
+++ routes/loggedInUser.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+router.get('/profile', (req, res, next) => {
+    res.json({
+        user: req.user,
+        token: req.query.secret_token
+    })
+});
+
+module.exports = router;
